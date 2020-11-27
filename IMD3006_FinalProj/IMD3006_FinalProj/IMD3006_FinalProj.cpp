92569953b3ecb68365f257845144da74d283bdbe
Ethan McIntyre <48802061+ethanmci@users.noreply.github.com>
2020-11-27 12:38:11 -0500

    main loops lets gooo

--- IMD3006_FinalProj/IMD3006_FinalProj/IMD3006_FinalProj.cpp
+++ IMD3006_FinalProj/IMD3006_FinalProj/IMD3006_FinalProj.cpp
@@ -24,6 +24,8 @@ int main()
 	string playerEntry; //should maybe replace player guess?
 	char playerGuess;
 	Player* currentPlayer = new Player();
+	string state = "NEW_GAME";
+	int difficulty = 0;
 
 	srand(time(NULL));
 
@@ -34,8 +36,10 @@ int main()
 		lineNum++;
 		//cout << word << endl;
 	}
-
-	cout << R"(
+	//gheto solution LOL, fix this later 
+	while (1) {
+		if (state == "NEW_GAME") {
+			cout << R"(
   ______  ____  _____          _____ _____ _   _  _____  
  |  ____|/ ____|/ ____|   /\   |  __ \_   _| \ | |/ ____| 
  | |__  | (___ | |       /  \  | |__) || | |  \| | |  __  
@@ -53,67 +57,84 @@ int main()
  | |__| |/ ____ \| |____| |___| |__| | \  /\  /  ____) |  
   \_____/_/    \_\______|______\____/   \/  \/  |_____/   
 )" << endl;
-	//gheto solution LOL, fix this later 
-	cout << "type 'start' to start!\n";
-	while (1) {
-		cin >> playerEntry;
-		if (playerEntry == "start") {
-			system("cls");
-			break;
-		}
-		else  if (playerEntry == "credits") {
-			//any librarys that need credit go here?
-		}
-		else {
-			cout << "invalid entry"; //temp fix again :)
-		}
-	}
-	//might wanna move these to the top ?
-	//vector<char> playerGuesses;
-	Enemy* testEnemy = new Enemy(wordArray[randomGen]);
-	string status;
-	while (1) {
-		cout << currentPlayer->getVis();
-		cout << endl << testEnemy->selWord << endl; //debugging output
-		cout << testEnemy->getVis();
-		if (testEnemy->pastGuesses.size() == 0)
-			cout << "a " << testEnemy->getEnemyType() << " approaches!\n";
-		cout << "word: ";
-		for (int x = 0; x < testEnemy->selWord.length(); ++x)
-		{
-			bool isGuessed = false;
-			for (int j = 0; j < testEnemy->pastGuesses.size(); j++) {
-				if (testEnemy->selWord[x] == testEnemy->pastGuesses[j]) {
-					cout << testEnemy->selWord[x] << " ";
-					isGuessed = true;
+			cout << "type 'start' to start!\n";
+			while (state == "NEW_GAME") {
+				cin >> playerEntry;
+				if (playerEntry == "start") {
+					system("cls");
+					state = "SELECT_DIFF";
+					break;
+				}
+				else  if (playerEntry == "credits") {
+					//any librarys that need credit go here?
+				}
+				else {
+					cout << "invalid entry"; //temp fix again :)
 				}
 			}
+		}
+		else if (state == "SELECT_DIFF") {
+			cout << "select difficulty:\n 1 = easy, 2 = normal, 3 = hard\n";
+			while (state == "SELECT_DIFF") {
+				cin >> difficulty;
+				if (difficulty > 0 && difficulty < 4) {
+					system("cls");
+					state = "GAME";
+					break;
+				}
+				else {
+					cout << "invalid difficulty!\n";
+				}
+			}
+		}
+		//might wanna move these to the top?
+		else if (state == "GAME") {
+			Enemy* testEnemy = new Enemy(wordArray[randomGen]);
+			string status;
+			while (1) {
+				cout << currentPlayer->getVis();
+				cout << endl << testEnemy->selWord << endl; //debugging output
+				cout << testEnemy->getVis();
+				if (testEnemy->pastGuesses.size() == 0)
+					cout << "a " << testEnemy->getEnemyType() << " approaches!\n";
+				cout << "word: ";
+				for (int x = 0; x < testEnemy->selWord.length(); ++x)
+				{
+					bool isGuessed = false;
+					for (int j = 0; j < testEnemy->pastGuesses.size(); j++) {
+						if (testEnemy->selWord[x] == testEnemy->pastGuesses[j]) {
+							cout << testEnemy->selWord[x] << " ";
+							isGuessed = true;
+						}
+					}
 
-			if (!isGuessed)
-				cout << "_ ";
-		}
-		cout << endl;
+					if (!isGuessed)
+						cout << "_ ";
+				}
+				cout << endl;
 
-		cin >> playerGuess;
-		if (testEnemy->selWord.find(playerGuess) != string::npos) {
-			status = "letter is in the word!\n";
-		}
-		else {
-			status = "letter is not in the word!\n";
-			currentPlayer->updateHealth(-1); //used to catch variable from going negative
-		}
+				cin >> playerGuess;
+				if (testEnemy->selWord.find(playerGuess) != string::npos) {
+					status = "letter is in the word!\n";
+				}
+				else {
+					status = "letter is not in the word!\n";
+					currentPlayer->updateHealth(-1); //used to catch variable from going negative
+				}
 
-		//checking the player's status
-		if (currentPlayer->health <= 0) {
-			status += "You have died\n";
-		}
-		else {
-			//hangman output here?
-			status += "You have " + to_string(currentPlayer->health) + " health left!\n";
-		}
+				//checking the player's status
+				if (currentPlayer->health <= 0) {
+					status += "You have died\n";
+				}
+				else {
+					//hangman output here?
+					status += "You have " + to_string(currentPlayer->health) + " health left!\n";
+				}
 
-		testEnemy->pastGuesses.push_back(playerGuess);
-		system("cls");
+				testEnemy->pastGuesses.push_back(playerGuess);
+				system("cls");
+			}
+		}
 	}
 
 
