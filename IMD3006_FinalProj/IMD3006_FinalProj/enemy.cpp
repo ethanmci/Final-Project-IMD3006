1d0805a13dcd6fa90669ac6e0164e3d86ed6f542
Ethan McIntyre <48802061+ethanmci@users.noreply.github.com>
2020-12-17 15:01:07 -0500

    another comment removed

--- IMD3006_FinalProj/IMD3006_FinalProj/enemy.cpp
+++ IMD3006_FinalProj/IMD3006_FinalProj/enemy.cpp
@@ -48,7 +48,6 @@ void Enemy::display()
 		cout << this->pastGuesses[g] << ", ";
 	}
 	cout << "]";
-	//cout << endl << this->selWord << endl; //debugging output
 	this->update();
 	cout << this->getVis();
 	if (this->pastGuesses.size() == 0)
